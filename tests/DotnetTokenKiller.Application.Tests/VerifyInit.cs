473f714b69c1bd9ada31b0339b54d9c11f0bff51
HandyS11 <valentin-clergue@orange.fr>
2026-03-09 19:18:47 +0100

    fix(ci): ensure all test related .txt files use UTF-8 with BOM

--- tests/DotnetTokenKiller.Application.Tests/VerifyInit.cs
+++ tests/DotnetTokenKiller.Application.Tests/VerifyInit.cs
@@ -1,5 +1,4 @@
 using System.Runtime.CompilerServices;
-using VerifyXunit;
 
 namespace DotnetTokenKiller.Application.Tests;
 
@@ -8,7 +7,7 @@ public static class VerifyInit
     [ModuleInitializer]
     public static void Initialize()
     {
-        Verifier.UseProjectRelativeDirectory("Snapshots");
+        UseProjectRelativeDirectory("Snapshots");
         VerifierSettings.IgnoreStackTrace();
         VerifierSettings.ScrubLinesWithReplace(line => line.Replace("\r\n", "\n", StringComparison.Ordinal));
     }
